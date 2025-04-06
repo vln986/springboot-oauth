349d30ffa062d5fe8d716565fa2e3d63a34f51a3
vln986 <laxminarayanaveggalam@gmail.com>
2025-04-06 20:37:48 +0530

    springboot oauth application

---
+++ src/main/java/com/oauth/springboot/SpringbootOauthApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOauthApplication.class, args);
+	}
+
+}
